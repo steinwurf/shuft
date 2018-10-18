bbcfe8aea9699aed1f6eb638da119ac4b827e5b0
Janus Heide <janus@steinwurf.com>
2018-10-18 14:12:43 +0200

    better handling of command line arguments

--- src/shuft/__main__.py
+++ src/shuft/__main__.py
@@ -17,36 +17,35 @@ async def cli(args):
         ''')
 
     parser.add_argument('localpath', type=str,
-                        help='Path to the local folder or file.')
+        help='Path to the local folder or file.')
 
     parser.add_argument('host', type=str,
-                        help='The name of the host for the remote.')
+        help='The name of the host for the remote.')
 
-    parser.add_argument('remotepath', type=str, help='''
-        The base path on the remote where the content will be uploaded.
-        ''')
+    parser.add_argument('remotepath', type=str,
+        help='The base path on the remote where the content will be uploaded.')
 
-    parser.add_argument('--port', type=int, help=''',
-        The port number on the remote.
-        ''')
+    parser.add_argument('--port', type=int, default=22,
+        help='The port number on the remote.')
 
-    parser.add_argument('--username', type=str,
-                        help='the username for logging in on the remote')
+    parser.add_argument('--known_hosts', type=str, default=(),
+        help='List of known hosts, if set to None accept any.')
 
-    parser.add_argument(
-        '--password', type=str, default=None,
+    parser.add_argument('--username', type=str, default=None,
+        help='the username for logging in on the remote')
+
+    parser.add_argument('--password', type=str, default=None,
         help='the password for logging in on the remote')
 
-    parser.add_argument(
-        '--compress', action='store_true',
+    parser.add_argument('--compress', action='store_true',
         help='Compress target folder or file before transmission.')
 
-    parser.add_argument(
-        '--version', action='store_true',
-        help='Print the version number.')
-
     args = parser.parse_args(args)
-    await shuft.upload(args)
+
+    if args.known_hosts == 'None':
+        args.known_hosts = None
+
+    await shuft.upload(**vars(args))
 
 
 if __name__ == "__main__":
--- src/shuft/shuft.py
+++ src/shuft/shuft.py
@@ -4,27 +4,32 @@
 import asyncssh
 import os
 import shutil
+import sys
 
 
-async def upload(args):
+async def upload(host, localpath, compress, **kwargs):
     """Compress, upload, remote uncompress and remove archieves."""
-    async with asyncssh.connect(args.host, username=args.username) as conn:
-        async with conn.start_sftp_client() as sftp:
 
-            if args.compress is False:
-                await sftp.put(args.localpath,
-                               remotepath=args.remotepath,
+    connect_args = {k:v for k,v in kwargs.items() if k in
+        ['port', 'known_hosts', 'username', 'password', ]}
+
+    put_args = {k:v for k,v in kwargs.items() if k in ['remotepath', ]}
+
+    async with asyncssh.connect(host, **connect_args) as conn:
+        async with conn.start_sftp_client() as sftp:
+            if compress is False:
+                await sftp.put(localpath2,
                                preserve=True,
-                               recurse=True)
+                               recurse=True,
+                               **put_args)
                 return
 
-            archieve = shutil.make_archive(
-                'upload', 'gztar', base_dir=args.localpath)
+            archieve = shutil.make_archive('upload', 'gztar', base_dir=localpath)
+            await sftp.put(archieve, preserve=True, **put_args)
 
-            await sftp.put(archieve, remotepath=args.remotepath, preserve=True)
+            await conn.run('tar -xf ' +
+                           os.path.join(kwargs['remotepath'],archieve) +
+                           ' -C ' + kwargs['remotepath'], check=True)
 
-            await conn.run('tar -xf ' + os.path.join(args.remotepath,archieve) +
-                           ' -C ' + args.remotepath, check=True)
-
-            await sftp.remove(os.path.join(args.remotepath,archieve))
+            await sftp.remove(os.path.join(kwargs['remotepath'],archieve))
             os.remove(archieve)
